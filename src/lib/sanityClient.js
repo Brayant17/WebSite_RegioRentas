fad614bf173f85d157d9d7548b22d950d24d3e2e
Brayant17 <Brayan_tsj@hotmail.com>
2025-07-22 10:33:23 -0600

    Cambio nombre variables entorno

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client'
 
 export const client = createClient({
-  projectId: import.meta.env.SANITY_PROJECT_ID, // <- cámbialo
-  dataset: import.meta.env.SANITY_DATASET,      // o el que estés usando
-  apiVersion: import.meta.env.SANITY_API_VERSION,   // puedes dejar esta versión
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, // <- cámbialo
+  dataset: import.meta.env.VITE_SANITY_DATASET,      // o el que estés usando
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,   // puedes dejar esta versión
   useCdn: true,               // true para producción (rápido)
 })
