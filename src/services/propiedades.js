5ec4a6aeb9ced23404da7d50652926b03b2e38bf
Brayant17 <Brayan_tsj@hotmail.com>
2025-08-08 17:57:11 -0600

    Example of property data

---
+++ src/services/propiedades.js
@@ -0,0 +1,55 @@
+
+export default function propiedades() {
+    return [
+        {
+            titulo: "Casa moderna con jardín",
+            direccion: "Calle 45 #123, Col. Reforma",
+            precio: 320000
+        },
+        {
+            titulo: "Departamento con vista al parque",
+            direccion: "Av. Central 789, Ciudad Jardín",
+            precio: 210000
+        },
+        {
+            titulo: "Casa colonial en el centro",
+            direccion: "Calle Mayor 12, Centro Histórico",
+            precio: 275000
+        },
+        {
+            titulo: "Penthouse en zona exclusiva",
+            direccion: "Av. del Sol 501, Zona Dorada",
+            precio: 500000
+        },
+        {
+            titulo: "Casa de campo con terreno",
+            direccion: "Camino Real 342, Las Lomas",
+            precio: 190000
+        },
+        {
+            titulo: "Departamento pequeño y acogedor",
+            direccion: "Calle 22 #98, Barrio Nuevo",
+            precio: 140000
+        },
+        {
+            titulo: "Casa familiar cerca de escuelas",
+            direccion: "Calle 5 #456, Los Pinos",
+            precio: 280000
+        },
+        {
+            titulo: "Loft moderno en zona industrial",
+            direccion: "Av. Industria 130, Parque Industrial",
+            precio: 230000
+        },
+        {
+            titulo: "Casa con piscina y jardín amplio",
+            direccion: "Calle Jardines 89, Residencial Vista",
+            precio: 360000
+        },
+        {
+            titulo: "Departamento en edificio nuevo",
+            direccion: "Av. Libertad 210, Centro Urbano",
+            precio: 200000
+        }
+    ]
+}
