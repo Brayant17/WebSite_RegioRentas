ee89b17711c7b4d7f4ea2947ef7706b08ede9cda
Brayant17 <Brayan_tsj@hotmail.com>
2025-10-15 18:15:02 -0600

    Normalize price in Card Property

--- src/components/property/PropertyCard.jsx
+++ src/components/property/PropertyCard.jsx
@@ -2,6 +2,13 @@
 import PropertySlider from "./PropertySlider.jsx"
 
 function PropertyCard({ id, property, slug }) {
+
+    const priceNormalized = property.price.toLocaleString("es-MX", {
+        style: "currency",
+        currency: "MXN",
+        minimumFractionDigits: 0,
+    });
+
     return (
         <article className="rounded-lg w-full">
             <div className="w-full aspect-[4/3] sm:aspect-[3/2] rounded-xl overflow-hidden">
@@ -12,7 +19,7 @@ function PropertyCard({ id, property, slug }) {
             <div className="py-1.5 px-4">
                 <span className="font-normal text-slate-700 font-display text-sm">{property.title}</span>
                 <p className="font-bold text-slate-700">{property.municipio} {property.colonia}</p>
-                <span className="font-bold text-slate-700">{property.price} MXN</span>
+                <span className="font-bold text-slate-700">{priceNormalized} MXN</span>
             </div>
         </article>
     )
