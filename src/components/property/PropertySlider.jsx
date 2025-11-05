71bb1bd543608e8b3e82005f1bfe8ed9f9338d25
Brayant17 <Brayan_tsj@hotmail.com>
2025-11-05 15:04:11 -0600

    style(component): se cambio los estilos a cada componente

--- src/components/property/PropertySlider.jsx
+++ src/components/property/PropertySlider.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
@@ -36,7 +35,7 @@ export default function PropertySlider({ images, name }) {
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 6l-6 6l6 6" /></svg>
             </button>
             <button className="btn-next absolute right-2 top-1/2 -translate-y-1/2 bg-white text-neutral-700 rounded-full w-10 h-10 flex items-center justify-center z-10 md:hidden group-hover:flex cursor-pointer">
-                <svg width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 6l6 6l-6 6" /></svg>
             </button>
         </Swiper>
     );
