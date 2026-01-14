dd787bfbd7db501895b904d0a92e91cd9e467922
Brayant17 <Brayan_tsj@hotmail.com>
2026-01-13 19:26:20 -0600

    feat(property): implement property repository

--- src/modules/realEstateBI/infrastructure/propertyRepo.ts
+++ src/modules/realEstateBI/infrastructure/propertyRepo.ts
@@ -1,21 +1,39 @@
 // infrastructure/propertyRepo.ts
 // crear el repo de supabase para las propiedades
 import type { PropertyDTO } from '../application/dtos/PropertyDTO';
-import type { Property } from '../domain/entities/Property';
+import { Property } from '../domain/entities/Property';
 import type { PropertyRepository } from '../domain/repositories/PropertyRepository';
 import { supabase } from '@/lib/supabaseClient';
 
 export class SupabasePropertyRepo implements PropertyRepository {
+
+    async insert(property: Property): Promise<void> {
+        const { error: error } = await supabase
+            .from('rental_properties')
+            .insert({
+                name: property.getName(),
+                address: property.getAddress(),
+                city: property.getCity() ?? "",
+                state: property.getState() ?? "",
+                postal_code: property.getPostalCode() ?? "",
+                active: property.isActive()
+            })
+        if (error) {
+            throw new Error(`Error saving property: ${error.message}`)
+        }
+    }
+
     async save(property: Property): Promise<void> {
         const { data, error } = await supabase
             .from('rental_properties')
-            .insert([
+            .upsert([
                 {
-                    name: property.name,
-                    address: property.address,
-                    city: property.city ?? "",
-                    state: property.state ?? "",
-                    postal_code: property.postalCode ?? "",
+                    id: property.id,
+                    name: property.getName(),
+                    address: property.getAddress(),
+                    city: property.getCity() ?? "",
+                    state: property.getState() ?? "",
+                    postal_code: property.getPostalCode() ?? "",
                     active: property.isActive()
                 }
             ]);
@@ -23,10 +41,12 @@ export class SupabasePropertyRepo implements PropertyRepository {
             throw new Error(`Error saving property: ${error.message}`);
         }
     }
+
     async getAll(): Promise<PropertyDTO[]> {
         const { data, error } = await supabase
             .from('rental_properties')
-            .select('*');
+            .select('*')
+            .order('created_at', {ascending: false});
         if (error) {
             throw new Error(`Error fetching properties: ${error.message}`);
         }
@@ -39,5 +59,28 @@ export class SupabasePropertyRepo implements PropertyRepository {
             postalCode: item.postal_code,
             active: item.active
         }));
+    }
+
+    async getById(id: string): Promise<Property> {
+        const { data, error } = await supabase
+            .from('rental_properties')
+            .select('*')
+            .eq('id', id);
+        if (error) {
+            throw new Error(`Error fetching property: ${error.message}`);
+        }
+        if (!data.length) {
+            throw new Error("Property not found");
+        }
+        const item = data[0];
+        return new Property(
+            item.id,
+            item.name,
+            item.address,
+            item.city,
+            item.state,
+            item.postal_code,
+            item.active
+        );
     }
 }
