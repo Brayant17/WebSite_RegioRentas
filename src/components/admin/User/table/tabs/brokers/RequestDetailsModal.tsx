9cee24c64af9d225fc5737ffc8105ce7c418bd5f
Brayant17 <Brayan_tsj@hotmail.com>
2026-02-27 20:32:53 -0600

    feat(user): ajusta el modal con la informacion del request

--- src/components/admin/User/table/tabs/brokers/RequestDetailsModal.tsx
+++ src/components/admin/User/table/tabs/brokers/RequestDetailsModal.tsx
@@ -1,40 +1,25 @@
-import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { Field, FieldGroup } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 export default function RequestDetailsModal({ open, request, onClose }: { open: boolean, request: any, onClose: () => void }) {
+
+    console.log("Request details:", request);
+
     return (
         <Dialog open={open} onOpenChange={onClose}>
-            <form>
-                <DialogContent >
-                    <DialogHeader>
-                        <DialogTitle>Detalles de la solicitud</DialogTitle>
-                        <DialogDescription>
-                            Aquí puedes ver los detalles de la solicitud de actualización a premium del usuario.
-                                Revisa la información cuidadosamente antes de aprobar o rechazar la solicitud.
-                            done.
-                        </DialogDescription>
-                    </DialogHeader>
-                    <FieldGroup>
-                        <Field>
-                            <Label htmlFor="name-1">Name</Label>
-                            <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-                        </Field>
-                        <Field>
-                            <Label htmlFor="username-1">Username</Label>
-                            <Input id="username-1" name="username" defaultValue="@peduarte" />
-                        </Field>
-                    </FieldGroup>
-                    <DialogFooter>
-                        <DialogClose asChild>
-                            <Button variant="outline">Cancel</Button>
-                        </DialogClose>
-                        <Button type="submit">Save changes</Button>
-                    </DialogFooter>
-                </DialogContent>
-            </form>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Detalles de la solicitud</DialogTitle>
+                    <DialogDescription>
+                        Gestionar la solicitud de afiliación de {request?.user_name}
+                    </DialogDescription>
+                </DialogHeader>
+                <div>
+                    <p><strong>Nombre del usuario:</strong> {request?.user_name} <Badge className="bg-yellow-50 text-yellow-700 dark:bg-yellow-950 dark:text-yellow-300" variant="outline">{request?.status}</Badge> </p>
+                    <p><strong>Email:</strong> {request?.user_email}</p>
+                    <p><strong>Fecha de solicitud:</strong> {new Date(request?.created_at).toLocaleDateString()}</p>
+                </div>
+            </DialogContent>
         </Dialog>
     );
 }
